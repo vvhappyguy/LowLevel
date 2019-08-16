3a4b6922185777e6d399b05add52d06f75e835a8
i.dyakin <i.dyakin@avsw.ru>
2019-08-16 15:43:34 +0300

    Added part of PAFISH to sources.

--- rdtsc_asm.cpp
+++ rdtsc_asm.cpp
@@ -22,8 +22,34 @@ void pafish_check()
     std::cout << std::dec << r2 - r1 << std::endl;
 }
 
+static inline unsigned long long rdtsc_diff_vmexit() {
+	unsigned long long ret, ret2;
+	unsigned eax, edx;
+	__asm__ volatile("rdtsc" : "=a" (eax), "=d" (edx));
+	ret  = ((unsigned long long)eax) | (((unsigned long long)edx) << 32);
+	/* vm exit forced here. it uses: eax = 0; cpuid; */
+	__asm__ volatile("cpuid" : /* no output */ : "a"(0x00));
+	/**/
+	__asm__ volatile("rdtsc" : "=a" (eax), "=d" (edx));
+	ret2  = ((unsigned long long)eax) | (((unsigned long long)edx) << 32);
+	return ret2 - ret;
+}
+
+int cpu_rdtsc_force_vmexit() {
+	int i;
+	unsigned long long avg = 0;
+	for (i = 0; i < 10; i++) {
+		avg = avg + rdtsc_diff_vmexit();
+	}
+	avg = avg / 10;
+    std::cout << "AVG: " << std::dec << avg << std::endl;
+	return (avg < 1000 && avg > 0) ? FALSE : TRUE;
+}
+
 int main()
 {
-    rdtsc();
+    std::cout << "RDTSC 1:" << std::hex << rdtsc() << std::endl;
+    cpu_rdtsc_force_vmexit();
+    std::cout << "RDTSC 2:" << std::hex << rdtsc() << std::endl;
     return 0;
 }
